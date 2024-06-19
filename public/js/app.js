cfeba16caa21e31bdfb1baccffc70c0fc2713e08
Jorge Leonardo Cardenas <leonardo.cardenas@rappi.com>
2024-06-19 15:17:12 -0500

    subiendo principio app

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,20 @@
-console.log('Hello from app.js\n Hello from app.js');
+window.alert(`
+    *********************************
+    *                           
+    *  Aplicacion de productos  
+    *                           
+    *********************************
+
+    Por favor digite su usuario y contraseña
+
+    -------------------------------------------
+    Nota:
+    - El usuario no debe tener espacios o caracteres especiales
+    - La contraseña debe tener al menos 8 caracteres y contener al menos un número
+    -------------------------------------------
+`);
+
+let usuario = prompt("Digite su usuario");
+let contrasena = prompt("Digite su contraseña");
+
+
