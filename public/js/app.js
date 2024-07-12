acd82e80e46cdbf2575484d3be599ce62ea8bbf6
Jorge Leonardo Cardenas <leonardo.cardenas@rappi.com>
2024-07-12 15:00:52 -0500

    subiendo validación del password

--- public/js/app.js
+++ public/js/app.js
@@ -5,5 +5,6 @@ window.addEventListener('load', ()=> {
 const login_pass = document.getElementById('floatingPassword');
 login_pass.addEventListener('change', () => {
   let login = new LoginView();
+  login.deleteAlerts();
   login.validatePassword(login_pass.value);
 });
--- views/login.js
+++ views/login.js
@@ -8,13 +8,21 @@ class LoginView{
         if(!pass){
             let loginError = document.createElement('div');
             loginError.classList.add('alert', 'alert-danger');
+            loginError.id = 'login-alert';
             loginError.innerHTML = `La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula, un número y un caracter especial`;
             login_pass.appendChild(loginError);
             return 0;
         }
         let loginSuccess = document.createElement('div')
         loginSuccess.classList.add('alert', 'alert-success');
+        loginSuccess.id = 'login-alert';
         loginSuccess.innerHTML = `Contraseña correcta`;
         login_pass.appendChild(loginSuccess);
+    }
+    deleteAlerts(){
+        const login_pass = document.getElementById('login-alert');
+        if(login_pass){
+            login_pass.remove();
+        }
     }
 }
